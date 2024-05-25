4fcfa236fc9a7377db5961f934dd51ec88ee88d3
EvErToN <evertonflorenciodasilva@outlook.com>
2024-05-25 16:40:36 -0300

    fazendo a conexão com os arquivos html

--- executar/app.py
+++ executar/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 
 @app.route('/home')
 def home ():
-    pass
+    return render_template('templates\home.html')
 @app.route('/cadastro')
 def cadastro():
     pass
