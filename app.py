08db1e7ecfe21e50ec3a23aceabdbe0128d357b6
EvErToN <evertonflorenciodasilva@outlook.com>
2024-05-25 16:57:03 -0300

    irei trabalhar na conexão com o banco de dados agora

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home ():
+    return render_template('home.html')
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+app.run(debug=True)
