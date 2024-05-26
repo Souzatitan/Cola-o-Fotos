6a1f1070f69279b44a8c16a874c2c23cddc4963f
EvErToN <evertonflorenciodasilva@outlook.com>
2024-05-26 20:38:50 -0300

    consegui resolver os erros que estava tendo na crianção do banco de dados

--- prepara_banco.py
+++ prepara_banco.py
@@ -5,7 +5,7 @@ from mysql.connector import errorcode
 
 try:
     conn=mysql.connector.connect(
-        host='127.0.0.1',
+        host='localhost',
         user='root',
         password='Efds11091999.',
       )
@@ -19,23 +19,23 @@ except mysql.connector.Error as err:
             
 cursor = conn.cursor()
 
-cursor.execute ("'DROP DATABASE IF EXIST `usuarios`;")
+cursor.execute ("DROP DATABASE IF EXISTS `usuario`;")
 cursor.execute ("CREATE DATABASE `usuario`;")
-cursor.execute ("USE `usuarios`;")   
+cursor.execute ("USE `usuario`;")   
 
-TABLLE = []
-TABLLE ['info_usuarios']=('''
-         CREATE TABLLE `info_usuarios`(
-             `nome` varchar(20) NOT NULL;
-             `email` nvarchar(50) NOT NULL;
-             `nickname` varhchar(8) NOT NULL;
-             `senha` varchar(100)NOT NULL
-             PRIMARY KEY (`nickname`)
-         )
-                )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+TABLE = {}
+TABLE ['info_usuarios']= '''
+    CREATE TABLE `info_usuarios` (
+        `nome` varchar(20) NOT NULL,
+        `email` varchar(50) NOT NULL,
+        `nickname` varchar(8) NOT NULL,
+        `senha` varchar(100) NOT NULL,
+        PRIMARY KEY (`nickname`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+'''
 
-for tabela_nome in TABLES:
-      tabela_sql = TABLES[tabela_nome]
+for tabela_nome in TABLE:
+      tabela_sql = TABLE[tabela_nome]
       try:
             print('Criando tabela {}:'.format(tabela_nome), end=' ')
             cursor.execute(tabela_sql)
@@ -46,27 +46,6 @@ for tabela_nome in TABLES:
                   print(err.msg)
       else:
             print('OK')
-
-
-# inserindo usuarios
-usuario_sql = 'INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
-usuarios = [
-      ("Bruno Divino", "BD", generate_password_hash("alohomora").decode('utf-8')),
-      ("Camila Ferreira", "Mila", generate_password_hash("paozinho").decode('utf-8')),
-      ("Guilherme Louro", "Cake", generate_password_hash("python_eh_vida").decode('utf-8'))
-]
-cursor.executemany(usuario_sql, usuarios)
-
-print(' -------------  Usuários:  -------------')
-for user in cursor.fetchall():
-    print(user[1])
-
-cursor.executemany(info, info)
-
-cursor.execute('select * from jogoteca.info')
-print(' -------------  info:  -------------')
-for info in cursor.fetchall():
-    print(info[1])
 
 conn.commit()
 
