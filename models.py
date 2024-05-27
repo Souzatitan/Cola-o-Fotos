6b24c0e37f177f42c9920740a766a297612ee225
EvErToN <evertonflorenciodasilva@outlook.com>
2024-05-26 21:03:56 -0300

    finalizei o banco de dados

--- models.py
+++ models.py
@@ -1,1 +1,8 @@
+from app import db
 
+
+class usuario(db.Model):
+    nome = db.column(db.String(20),nullable=False)
+    email = db.column(db.String(50),unique=True,nullable=False)
+    nickname = db.column(db.String(8),unique=True,primary_key=True)
+    senha = db.collumn(db.interger,unique=True, nullable=False)
